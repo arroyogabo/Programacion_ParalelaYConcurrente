a41187f3ab10d6a4f0c692f98a92f594ba74e5d5
oigresagetro <31581117+oigresagetro@users.noreply.github.com>
2019-05-02 13:04:18 -0600

    Main

---
+++ Proyecto#1/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "Simulador.h"
+#include "Tortuga.h"
+#include "Contador.h"
+
+using namespace std;
+
+int main() {
+
+	Simulador s;
+	s.simular(200);
+
+	cin.ignore();
+	return 0;
+}
