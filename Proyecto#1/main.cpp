f40d27838494952a5fac5784a4e1d8a95b1a8178
oigresagetro <31581117+oigresagetro@users.noreply.github.com>
2019-05-06 17:15:19 -0600

    Inclusion de Aleatorizador

--- Proyecto#1/main.cpp
+++ Proyecto#1/main.cpp
@@ -2,12 +2,14 @@
 #include "Simulador.h"
 #include "Tortuga.h"
 #include "Contador.h"
+#include "Aleatorizador.h"
 
 using namespace std;
 
 int main() {
 
 	Simulador s;
+	Aleatorizador::inicializar_generador_random();
 	s.simular(200);
 
 	cin.ignore();
