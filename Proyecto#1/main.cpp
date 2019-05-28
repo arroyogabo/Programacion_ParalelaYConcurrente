4869c40ff94ebd00ee38e1d2b60381e978f87710
oigresagetro <31581117+oigresagetro@users.noreply.github.com>
2019-05-27 18:20:01 -0600

    Cuadro comparativo

--- Proyecto#1/main.cpp
+++ Proyecto#1/main.cpp
@@ -10,8 +10,6 @@ using namespace std;
 
 int main() {
 
-	int thread_count = 0;
-	int cntTortugas = 0;
 	int totalTics = 372;
 	int max_thread_count = omp_get_max_threads();
 	int estimadoCuadrantes = 0;
@@ -21,56 +19,97 @@ int main() {
 
 	ifstream entrada;
 	ofstream salida;
+	ofstream cuadro;
 	Simulador s;
+	vector<double> tiemposPromedio;
 	vector< vector<int> > experimentos;
 	Aleatorizador::inicializar_generador_random();
-	/*
-	while (thread_count < 1 && cntTortugas <= 0) {
-		cout << "Ingrese la cantidad de tortugas que desea simular: " << endl;
-		cin >> cntTortugas;
-		cout << "Ingrese la cantidad de hilos con los que desea trabajar: " << endl;
-		cout << "Cantidad maxima recomendada: " << max_thread_count << endl;
-		cin >> thread_count;
-	}
-
-	cout << "Simulando " << cntTortugas << " tortugas con " << thread_count << " hilos" << endl;
 	
-	wtime = omp_get_wtime();
-	s.simular(totalTics, cntTortugas, thread_count);
-	wtime = omp_get_wtime() - wtime;
-	cout << "En un tiempo de duracion: " << wtime << " segundos." << endl;
-
-	estimadoCuadrantes = s.obtEstimacionXcuadrantes();
-	estimadoTV = s.obtEstimacionXtransectosSobreBerma();
-	estimadoTPB = s.obtEstimacionXtransectoHorizontal();
-
-	cout << "\nGenerando archivo con resultados..." << endl;
 	
-	salida.open("resultado.csv");
-
-	salida << cntTortugas << "," << thread_count << "," << estimadoCuadrantes << "," << estimadoTV << "," << estimadoTPB << "," << wtime;
-
-	salida.close();
-
-	cout << "\nArchivo generado." << endl;
-	*/
 	///CODIGO PARA CORRER TODOS LOS EXPERIMENTOS
 	
 	s.leerDatos(entrada, experimentos, "experimentos.csv");
-	
+	salida.open("resultado.csv");
 	for (int i = 0; i < experimentos.size(); ++i) {
 		int cntTortugas = experimentos[i][2];
 		for (int j = 3; j < experimentos[i].size(); ++j) {
 			int thread_count = experimentos[i][j];
-			Simulador * s1 = new Simulador();
-			cout << "Experimento con: " << cntTortugas << " tortugas y " << thread_count << " hilos" << endl;
+			int contador = 0;
+			double wtime = 0.0;
+			salida << "===================== EXPERIMENTO # " << i+1 << " ==========================\n";
+			while (contador < 10) {
+				double wtime_local = 0.0;
+				Simulador * s1 = new Simulador();
 
-			s1->simular(totalTics, cntTortugas, thread_count);
-			delete s1;
+				cout << "\nExperimento con: " << cntTortugas << " tortugas y " << thread_count << " hilos" << endl;
+
+				s1->simular(totalTics, cntTortugas, thread_count);
+				wtime += s1->getWtime();
+				wtime_local += s1->getWtime();
+				estimadoCuadrantes = s1->obtEstimacionXcuadrantes();
+				estimadoTV = s1->obtEstimacionXtransectosSobreBerma();
+				estimadoTPB = s1->obtEstimacionXtransectoHorizontal();
+
+				salida << contador+1 << " Cantidad de Tortugas simuladas: " << cntTortugas << "  ";
+				salida << "Cantidad de hilos: " << thread_count << "  ";
+				salida << "Cuadrantes: " << estimadoCuadrantes << "  ";
+				salida << "TV: " << estimadoTV << "  ";
+				salida << "TPB: " << estimadoTPB << "  ";
+				salida << "Duracion (s): " << wtime_local << "\n";
+
+				delete s1;
+				++contador;
+			}
+			cout << "Tiempo de duracion total entre las 10 corridas: " << wtime << endl;
+			cout << "\n" << endl;
+			salida << "Tiempo total de las 10 corridas: " << wtime << "  ";
+			double tPromedio = wtime / 10.0;
+			tiemposPromedio.push_back(tPromedio);
+			salida << "Tiempo promedio por corrida: " << tPromedio << "\n\n";
 		}
-		cout << endl;
 	}
 	
+	salida.close();
+	cout << "\nArchivo generado." << endl;
+
+	/*
+	cuadro.open("Cuadro_Comparativo.txt");
+
+	cuadro << "# Hilos\t\t\t" << "Experimento #1\t\t\t" << "Experimento #2\t\t\t" << "Experimento #3\n";
+	for (int i = 0; i < experimentos.size(); ++i) {
+		double wtime = 0.0;
+		double aceleracion = 0.0;
+		if (i == 0) {
+			wtime = tiemposPromedio[i];
+			aceleracion = wtime;
+		}else {
+			if (i < 3) {
+				wtime = tiemposPromedio[i];
+				aceleracion = tiemposPromedio[i - 1] / wtime;
+			}else {
+				if (i < 6) {
+					if (i == 3) {
+						wtime = tiemposPromedio[i];
+						aceleracion = wtime;
+					}
+					wtime = tiemposPromedio[i];
+					aceleracion = tiemposPromedio[i - 1] / wtime;
+				}else {
+					if (i == 6) {
+						wtime = tiemposPromedio[i];
+						aceleracion = wtime;
+					}else {
+						wtime = tiemposPromedio[i];
+						aceleracion = tiemposPromedio[i - 1] / wtime;
+					}
+				}
+			}
+		}
+
+	}
+
+	cuadro.close();
+	*/
 
 	cin.ignore();
 	cin.ignore();
