66542dcaecef69c045f492ef583546a7341ae464
oigresagetro <31581117+oigresagetro@users.noreply.github.com>
2019-07-06 15:13:05 -0600

    Update main_arribada.py

--- Proyecto#2_MPI/main_arribada.py
+++ Proyecto#2_MPI/main_arribada.py
@@ -3,6 +3,7 @@
 import sys
 import csv
 from enum import Enum
+from mpi4py import MPI
 
 from Tortuga import Tortuga
 from Contador import Contador
@@ -51,6 +52,13 @@ def read_data(file_path, tn):
 	
 def main():
 	
+	comm = MPI.COMM_WORLD
+	pid = comm.rank
+	proc_count = comm.size
+	
+	global_conteo_tv = 0
+	global_conteo_c = 0
+	
 	dts_c = read_data("archivos\cuadrantes.csv", Tipos_numeros.int)		# datos de los cuadrantes.
 	dts_ct = read_data("archivos\comportamiento_tortugas.csv", Tipos_numeros.float)		# datos del comportamiento de tortugas.
 	dts_tv = read_data(r"archivos\transectos_verticales.csv", Tipos_numeros.int)
@@ -66,16 +74,44 @@ def main():
 	Simulador.inicializar_transecto_berma(dts_tpb)
 	Simulador.inicializar_transectos_verticales(dts_tv)
 	Simulador.inicializar_cuadrantes(dts_c)
-	Simulador.inicializar_arribada(dts_ct, 10) #dts_exp[0][2]
+	Simulador.inicializar_arribada(dts_ct, 7000) #dts_exp[0][2]
 	
 	
-	Simulador.simular(372)
-	print(Tortuga.desactivarse(Tortuga, 0.5))
-	conta = Contador()
-	print(conta.obt_posicion())
-	conta.avanzar()
-	print(conta.obt_posicion())
+	conteo_tv, conteo_c = Simulador.simular(372)
+	global_conteo_tv = comm.reduce(conteo_tv, op=MPI.SUM)
+	global_conteo_c = comm.reduce(conteo_c, op=MPI.SUM)
+	
+	if pid == 0:
+	
+		##ESTIMACION TV
+		estimacion_tv = 0.0
+		area_observacion = 0.0
+		long_tv = 0.0
+		total_tics = 372.0
+		w = 2.0
+		m = total_tics // (dts_tv[0][1]*2.0) #El denominador es el tiempo necesario para un muestreo. 15*2(conteo 15 y esperando 15) 
+		pt = 64.8	#tiempo que duran las tortugas en el transecto.
+		for i in range(len(dts_terreno)):
+			area_observacion = area_observacion + (dts_terreno[i][3]*100)
+			long_tv = long_tv + ((dts_terreno[i][3]))
+			
+		estimacion_tv = (area_observacion*total_tics)
+		estimacion_tv = estimacion_tv / (2.0*w*m*long_tv)
+		estimacion_tv = estimacion_tv * (global_conteo_tv/pt)
+		
+		##ESTIMACION CUADRANTES
+		area_observacion = 0.0
+		for i in range(len(dts_terreno)):
+			area_observacion = area_observacion + (dts_terreno[i][3])
+		estimacion_c = 0.0
+		area_cuadrantes = 10.0 * 10.0
+		m = total_tics // (dts_ct[0][1]*2.0)
+		estimacion_c = global_conteo_c * 1.25 
+		estimacion_c = estimacion_c * (area_cuadrantes/area_observacion) 
+		estimacion_c = estimacion_c * total_tics
+		estimacion_c = estimacion_c / (1.08 * m)
+		print(estimacion_c)
+		print(estimacion_tv)
 	## OJO: el archivo esta en una carpeta "archivos"
 	
 main()
-
